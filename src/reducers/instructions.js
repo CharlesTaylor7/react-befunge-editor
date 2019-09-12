3a254632d472a220407e957238b94bcf27ff001b
Charles Taylor <charlestaylor95@gmail.com>
2019-09-11 20:50:32 -0400

    Add instructions.js

---
+++ src/reducers/instructions.js
@@ -0,0 +1,33 @@
+import * as R from 'ramda';
+import * as Stack from '../utilities/stack';
+
+export const executeCurrent = (state) => {
+  const instruction = state.grid[`${state.position.x}-${state.position.y}`];
+  const number = Number(instruction);
+
+  if (Number.isInteger(number)) {
+    return R.mergeRight(state, { stack: Stack.cons(number, state.stack) });
+  }
+
+  switch (instruction) {
+    case ' ':
+    case undefined:
+      return state;
+    case '+':
+      return runBinaryOpOnStack((a, b) => a + b);
+    case '-':
+      return runBinaryOpOnStack((a, b) => a - b);
+    case '*':
+      return runBinaryOpOnStack((a, b) => a * b);
+    case '/':
+      return runBinaryOpOnStack((a, b) => a / b);
+    default:
+        console.error(`Unable to interpret unknown instruction: ${instruction}`);
+  }
+}
+
+const runBinaryOpOnStack = (op, state) => {
+  const [ a, b, rest ] = Stack.pop(state.stack, 2);
+  const combined = op(a, b);
+  return R.mergeRight(state, { stack: Stack.cons(combined, rest) });
+}
